d9deead32642130d0973382957bf971e8ffd8008
Podence <podence23@gmail.com>
2023-02-12 15:42:04 -0300

    req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -35,7 +35,27 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    content = Selector(text=html_content)
+    new = {
+        "url": content.css("link[rel='canonical'] ::attr(href)").get(),
+        #  https://stackoverflow.com/questions/52849274/getting-the-current-url-page-ref-scrapy
+        "title": content.css(".entry-title ::text").get().strip(),
+        "timestamp": content.css(
+            ".cs-bg-dark > ul > li.meta-date ::text"
+        ).get(),
+        "writer": content.css("li.meta-author > span.author > a ::text").get(),
+        "reading_time": int(content.css(
+            "li.meta-reading-time ::text"
+        ).get().split(" ")[0]),
+        "summary": "".join(content.css(
+            "div.entry-content > p:nth-of-type(1) *::text"
+        ).getall()).strip(),
+        #  https://developer.mozilla.org/pt-BR/docs/Web/CSS/:nth-of-type
+        "category": content.css(
+            ".cs-bg-dark > div > div > a > span.label ::text"
+        ).get()
+    }
+    return new
 
 
 # Requisito 5

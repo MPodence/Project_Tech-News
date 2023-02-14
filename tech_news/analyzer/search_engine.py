7f9ba9548d6c7fb1a6401bb025bd62c88255aebb
Podence <podence23@gmail.com>
2023-02-14 14:44:05 -0300

    req 7 e 9 que sao extremamente semelhantes

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title: str):
+    data = []
+    # https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362
+    # ref para case-insensitive como opção de regex
+    news = search_news({"title": {"$regex": title, "$options": 'i'}})
+    for new in news:
+        data.append((new["title"], new["url"]))
+    return data
 
 
 # Requisito 8
@@ -9,5 +18,9 @@ def search_by_date(date):
 
 
 # Requisito 9
-def search_by_category(category):
-    """Seu código deve vir aqui"""
+def search_by_category(category: str):
+    data = []
+    news = search_news({"category": {"$regex": category, "$options": 'i'}})
+    for new in news:
+        data.append((new["title"], new["url"]))
+    return data
